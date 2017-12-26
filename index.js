0dfff9cfa2c04626a34f60ba7e39775389b8515a
kuroneko0441 <kuroneko0441@gmail.com>
2017-12-27 00:36:30 +0900

    implementing postgresql

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const { Pool } = require("pg");
 
 const port = process.env.PORT || 5000;
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL + "?ssl=true"
+});
 
 express()
     .use((req, res, next) => {
@@ -8,4 +12,15 @@ express()
         next();
     })
     .get('/', (req, res) => res.send("server running"))
+    .get('/db', (req, res) => {
+        pool.query("SELECT * FROM test_table", (err, result) => {
+            if (err) {
+                console.error(err);
+                res.send("DB Error occured")
+            } else {
+                res.json(result.rows);
+            }
+            pool.end();
+        })
+    })
     .listen(port, () => console.log(`Listening on ${port}`));
